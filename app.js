6a1212ab3747874e4fe5a677da82ad358d6fd8a7
Davihan11 <davihan11@gmail.com>
2022-12-24 10:47:20 +0100

    Added basic operation substract

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
-let numbers = [5,4,3,-2,1]
+let numbers = [5,4,3,2,1]
 
 
 //declaring basic operation functions
-function sum(numbers){
+
+//function sum
+function doAdd(numbers){
     const sum = numbers.reduce((total, next)=> total + next, 0)
     console.log(sum)
     return sum
 }
 
+//function substract
+function doSubstract(numbers){
+    const substract = numbers.reduce((total,next) => total - next,0)
+    console.log(substract)
+}
+
+//
 //testing operations
-sum(numbers)
+doAdd(numbers)
+doSubstract(numbers)
